76433bc16f1d111967cf5d351f1e44c16d601a30
Eugene Protsenko <guitaaarpro@gmail.com>
2024-04-17 16:03:16 +0200

    Solution

---
+++ settings.py
@@ -0,0 +1,13 @@
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "temperature-management-api"
+
+    DATABASE_URL: str = "sqlite+aiosqlite:///./temperature-management-api.db"
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+settings = Settings()
